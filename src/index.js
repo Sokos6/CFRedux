6c49ee44256406a6f9ae169bca5b0bca1131095c
Sokos6 <will.sokolowski@gmail.com>
2020-09-05 22:23:20 -0500

    working state outside react

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import store from './store/index';
+window.store = store;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,13 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+  if (action.type === 'ADD_POST') {
+    return Object.assign({}, state, {
+      posts: state.posts.concat(action.payload),
+    });
+  }
   return state;
-}
+};
 
 const store = createStore(reducer);
 
