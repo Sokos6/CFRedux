83dd65a70cc282d46037900b5ee5c9474cbacf62
Sokos6 <will.sokolowski@gmail.com>
2020-09-06 01:27:37 -0500

    dispatch to props and intergration done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import store from './store/index';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = { value: '', postId: 2 };
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.dispatch({
+      type: 'ADD_POST',
+      payload: { id: this.state.postId, title: this.state.value },
+    });
+    this.setState({ postId: this.state.postId + 1 });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <div>
+            <button type='submit' onClick={this.handleSubmit}>
+              Submit
+            </button>
+          </div>
+        </form>
+        <ul>
+          {this.props.posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatch,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
